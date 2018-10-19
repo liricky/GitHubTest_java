4a2cafa30dc97a2a75fa8aa776d16d1ca61758d8
liricky <15901912427@163.com>
2018-10-19 22:12:38 +0800

    IDEA整合GitHub测试

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import static java.lang.Math.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int hit, hil, a = 0;
+        hit = hil = 2;
+        System.out.println("Hello World!");
+        System.out.println((5 * 3) / (double) 4);
+        System.out.print(hit);
+        System.out.println(" " + hil);
+        a += 2;
+        System.out.println(a);
+        System.out.println(pow(2, 3));
+    }
+}
